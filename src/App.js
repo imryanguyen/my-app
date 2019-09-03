68b3f57f99a257f306dd3b124ae06c1283a7e696
Duc Anh (Ryan) Nguyen <nduc.anh@gmail.com>
2019-09-03 22:46:49 +0700

    regular commit

--- src/App.js
+++ src/App.js
@@ -52,16 +52,14 @@ class App extends Component{
 
     let persons = null;
 
-    if (this.state.showPersons) {
+    if ( this.state.showPersons ) {
       persons = (
         <div>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age='9'
-          changed={this.nameChangeHandler}> My hobbies: Racing </Person>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name}
+              age={person.age}  />
+          })}     
         </div> 
       );
     }
